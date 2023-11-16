6af42432952ee9c0ca0dc68527b11deab6b99504
anambro022 <anambro022@student.wethinkcode.co.za>
2023-11-16 17:33:18 +0200

    New Review class: Added a basic Review class.

--- src/main/java/project/antonio/movies/Review.java
+++ src/main/java/project/antonio/movies/Review.java
@@ -7,21 +7,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
-@Document(collection = "movies")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Movie {
+public class Review {
     @Id
     private ObjectId id;
-    private String imdbId;
-    private String title;
-    private String releaseDate;
-    private String trailerLink;
-    private String poster;
-    private List<String> genres;
-    private List<String> backdrop;
-    private List<Review> reviewIds;
+    private String body;
 }
