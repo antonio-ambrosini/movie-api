a75c7be89539b9d5c9e9677b36346187352ebedd
anambro022 <anambro022@student.wethinkcode.co.za>
2023-11-17 13:49:10 +0200

    Added New Controller: Created basic movie controller class.

---
+++ src/main/java/project/antonio/movies/MovieController.java
@@ -0,0 +1,16 @@
+package project.antonio.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("This will display all the movies!", HttpStatus.OK);
+    }
+}
